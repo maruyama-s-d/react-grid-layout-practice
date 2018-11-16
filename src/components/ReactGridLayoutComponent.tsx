ea47573b123c08f332ef388dcef1f242b9862812
maruyama-s-d <maruyama.shotaro@donuts.ne.jp>
2018-11-16 11:48:14 +0900

    try to save previous layout

--- src/components/ReactGridLayoutComponent.tsx
+++ src/components/ReactGridLayoutComponent.tsx
@@ -9,67 +9,41 @@ import * as RGL from 'react-grid-layout';
 import { WidthProvider } from 'react-grid-layout';
 const ReactGridLayout = WidthProvider(RGL);
 
-interface IRGLLayout extends RGL.Layout {
-    i: string;
-    x: number;
-    y: number;
-    w: number;
-    h: number;
-    minW?: number;
-    maxW?: number;
-    static?: boolean;
-}
-
-interface IRGLState {
-    layout: IRGLLayout[];
-}
-
-// export class ReactGridLayoutComponent extends React.Component<{}, IReactGridLayoutState> {
-//     constructor() {
-//         super({});
-
-//         const layoutItems: IReactGridLayoutItem[] = [
-//             { i: 'a', x: 0, y: 0, w: 1, h: 2, static: false },
-//             { i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-//             { i: 'c', x: 4, y: 0, w: 1, h: 2 }
-//         ];
-
-//         this.state = { layout: layoutItems, loading: true };
-//     }
-
-//     public render() {
-//         return <div>
-//             <ReactGridLayout className="layout" layout={this.state.layout} cols={12} rowHeight={30} width={1200}>
-//                 <div key="a">a</div>
-//                 <div key="b">b</div>
-//                 <div key="c">c</div>
-//             </ReactGridLayout>
-//         </div>;
-//     }
-// }
-
-
-interface IGRLProps{
-    claccName: string,
+interface IProps extends RGL.ReactGridLayoutProps {
+    className: string,
     items: number,
     cols: number,
     rowHeight: number,
     verticalCompact: boolean,
+    onLayoutChange: (layout: ILayout[]) => void,
 }
 
-export class NoCompactingLayout extends React.PureComponent<IGRLProps, IRGLState> {
-    public static defaultProps: Partial<IGRLProps> = {
-        claccName: "layout",
+interface IState{
+    layout: ILayout[];
+}
+
+interface ILayout extends RGL.Layout {
+    i: string,
+    x: number,
+    y: number,
+    w: number,
+    h: number,
+}
+
+export class NoCompactingLayout extends React.PureComponent<IProps, IState> {
+    public static defaultProps : Partial<IProps> = {
         items: 50,
+        className: "layout",
         cols: 12,
         rowHeight: 30,
         verticalCompact: false,
     }
 
-    constructor(props: IGRLProps) {
+    constructor(props: IProps) {
         super(props);
         const layout = this.generateLayout();
         this.state = { layout };
+        this.onLayoutChange = this.onLayoutChange.bind(this);
     }
 
     public generateDOM() {
@@ -82,7 +56,7 @@ export class NoCompactingLayout extends React.PureComponent<IGRLProps, IRGLState
         });
     }
 
-    public generateLayout(): IRGLLayout[] {
+    public generateLayout(): ILayout[] {
         const p = this.props;
         return _.map(new Array(p.items), (item, i) => {
             const y = _.result(p, "y", Math.ceil(Math.random() * 4) + 1);
@@ -96,9 +70,14 @@ export class NoCompactingLayout extends React.PureComponent<IGRLProps, IRGLState
         });
     }
 
+    public onLayoutChange(layout: ILayout[]) {
+        // this.props.onLayoutChange(layout);
+        this.setState({layout});
+    }
+
     public render() {
         return (
-            <ReactGridLayout layout={this.state.layout} {...this.props}>
+            <ReactGridLayout layout={this.state.layout} onLayoutChange={this.onLayoutChange} {...this.props}>
                 {this.generateDOM()}
             </ReactGridLayout>
         )
